1bc9b887b3de486bc0b09f3e42d1f39337ae4337
anurag1001001 <anuragtripathi23may@gmail.com>
2020-12-26 16:57:24 +0530

    add reducers

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+//  Reducer always state return krta h, and it could be a new state or old state then store hota h wo returned state se compare krta h and if change kuch hua hota h toh current state ko returned state se change kr deta h
+
+
+import React from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
+
+
+function App() {
+  return (
+    <div className= 'App'>
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {data.map(movie => (
+            <MovieCard movie ={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+// Reducers in Redux is a Pure function. And in functional programming a function is a pure function if it satisfied 3 conditions, i.e 1-- For the same input Output should be the same everyTime means that no matters how many times i call pure function with a different input but same type(same parameter, and type) Output should be same. 2-- And the second condition is that  function shouuld not use value of another variable that are not define in their local scope and should use argument that are provided to the function
+
+//  reducers function takes two parameter one is current state(and this is not going to be  undefined that's why i initialise with an empty array) and the 2nd parameter would be action(type of action)
+
+// reducers has to return something whether it could be a new state or something else
+
+
+export default function movies(state=[], action){
+    if(action.type == 'ADD_MOVIES'){
+        return action.movies;
+    }
+    return state;
+}
